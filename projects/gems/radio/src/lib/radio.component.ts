45584c57855ce70162750f64aa19c3cf92682189
Jaosn <jason.cardenas@intelity.com>
2022-11-09 17:24:03 -0500

    gitignore

---
+++ projects/gems/radio/src/lib/radio.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-radio',
+  template: `
+    <p>
+      radio works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RadioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/gems/radio/src/lib/radio.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RadioComponent } from './radio.component';
+
+
+
+@NgModule({
+  declarations: [
+    RadioComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    RadioComponent
+  ]
+})
+export class RadioModule { }
