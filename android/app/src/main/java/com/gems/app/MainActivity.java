b3ba29e8906b049d615f1c3408c548d5d8039fb3
Jaosn <jason.cardenas@intelity.com>
2022-12-07 13:39:09 -0500

    installed capacitor

---
+++ android/app/src/main/java/com/gems/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gems.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gems.app',
+  appName: 'Gems',
+  webDir: 'dist/dashboard',
+  bundledWebRuntime: false
+};
+
+export default config;
